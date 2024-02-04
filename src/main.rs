4aa089cd32bb3862305b7f17c1b2a5f4079ca7af
Pavel Yudakov <pyudakov@itoolabs.com>
2024-02-04 23:12:38 +0300

    add colors to board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// struct Piece {
+//     color: String,
+// }
+
 fn main() {
-    println!("Hello, world!");
+    const BLACK_BACK: &str = "\x1b[101m";
+    const WHITE_BACK: &str = "\x1b[107m";
+    const RESET_COLOR: &str = "\x1b[0m";
+
+    //♚♛♜♝♞♟︎♟︎♔♕♖♗♘♙
+
+    for row in 1..9 {
+        for cell in 1..9 {
+            let color = if (row + cell) % 2 == 0 {
+                BLACK_BACK
+            } else {
+                WHITE_BACK
+            };
+            print!("{}   {}", color, RESET_COLOR)
+        }
+        println!("")
+    }
 }
